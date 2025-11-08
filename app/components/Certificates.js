165bfce839ccc5f29498e477ce7827765c99217d
Rodrigo Campos <141381204+usernumber89@users.noreply.github.com>
2025-11-07 21:40:16 -0600

    Update Certificates.js

--- app/components/Certificates.js
+++ app/components/Certificates.js
@@ -37,7 +37,7 @@ export default function Certificates() {
       issuer: 'Santander Open Academy',
       date: 'April 2025',
       credentialId: 'OA-2025-0415001016672',
-      link: '/liderezgo.pdf',
+      link: '/liderazgo.pdf',
       skills: ['Teamwork', 'Leadership Development', 'Leadership']
     },
     {
